b78ab08c447851dd86f9706232721855f28c4f12
Seoyoung Hwang <dev.seoyoung.hwang@gmail.com>
2023-07-06 12:41:41 -0600

    Install firebase Add initialize setting using .env file to hide key value Put .env file in ignore file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from './firebase';
+console.log(firebase);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
