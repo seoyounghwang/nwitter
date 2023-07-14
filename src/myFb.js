c6050b5a26afbf1d9a51d5d16715ae3983000f7b
Seoyoung Hwang <dev.seoyoung.hwang@gmail.com>
2023-07-14 11:50:09 -0600

    myFb.js Import createUserWithEmailAndPassword signInWithEmailAndPassword from firebase
    Auth.js
    Use imported function when submit the from
    (create or sign in)
    
    Create toggle function to toggle sign in/ create mode

--- src/myFb.js
+++ src/myFb.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -21,5 +21,6 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+export {createUserWithEmailAndPassword, signInWithEmailAndPassword};
 
 export default app;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,3 +1,54 @@
-import React from "react";
+import { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'myFb';
+import React, { useState } from "react";
 
-export default () => <span>Auth</span>;
+
+const Auth = () => {
+    const [form, setForm] = useState({email: "", password: ""});
+    const [isNewAccount, setIsNewAccount] = useState(true);
+    const [error, setError] = useState("");
+    const onChange = ({target: {name, value}})=> setForm({...form, [name]: value})
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            if(isNewAccount) {
+                const data = await createUserWithEmailAndPassword(auth, form.email, form.password)
+                .then((userCredential) => {
+                    const user = userCredential.user;
+                    console.log(user);
+                })
+            } else {
+                await signInWithEmailAndPassword(auth, form.email, form.password)
+                .then((userCredential)=> {
+                    const user = userCredential.user;
+                })   
+            }
+        } catch (err) {
+            setError(err.message); 
+        }
+        
+    }
+
+    const toggleAccount = () => setIsNewAccount(prev => !prev);
+
+    return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <input type='text' name='email' placeholder='Email' required onChange={onChange}/>
+            <input type='password' name='password' placeholder='Password' required onChange={onChange}/>
+            <input type='submit' value={isNewAccount ? 'Create New Account':'Sign in'} />
+            {error}
+        </form>
+        <span onClick={toggleAccount}>{!isNewAccount ? 'Create New Account':'Sign in'}</span>
+        
+        <div>
+            <button>Continue with Google</button>
+            <button>Continue with Github</button>
+        </div>
+    </div>
+
+    )
+
+};
+
+export default Auth;
