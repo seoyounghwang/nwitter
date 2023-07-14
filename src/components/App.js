81f4e710b529a56d30fbadf664610a4e48a8b09c
Seoyoung Hwang <dev.seoyoung.hwang@gmail.com>
2023-07-14 11:56:55 -0600

    Init firebase based on the onAuthStateChanged func

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import AppRouter from 'components/Router';
 import {auth} from 'myFb';
 
 function App() {
 
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
+  useEffect(()=> {
+    auth.onAuthStateChanged((user)=>{
+      if(user) {
+        setIsLoggedIn(true)
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  },[])
   return (
     <div className="App">
-      <AppRouter isLoggedIn={isLoggedIn}/>
+      {init ? <AppRouter isLoggedIn={isLoggedIn}/>: "Initializing...."}
     </div>
   );
 }
